3567455b5fa00057d9130acce36a774a1d6911a4
Mohammad SF <mohammad7293@gmail.com>
2024-11-28 18:59:50 +0330

    added findUsersByIds method to users service with its tests

--- src/users/users.service.spec.ts
+++ src/users/users.service.spec.ts
@@ -2,22 +2,31 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 
-const userArray = [
+const mockUsers = [
   {
+    id: 1,
     userName: 'userName1',
     firstName: 'firstName #1',
     lastName: 'lastName #1',
   },
   {
+    id: 2,
     userName: 'userName2',
     firstName: 'firstName #2',
     lastName: 'lastName #2',
   },
+  {
+    id: 3,
+    userName: 'userName3',
+    firstName: 'firstName #3',
+    lastName: 'lastName #3',
+  },
 ];
 
 const oneUser = {
+  id: 1,
   userName: 'userName1',
   firstName: 'firstName #1',
   lastName: 'lastName #1',
@@ -34,7 +43,7 @@ describe('UserService', () => {
         {
           provide: getRepositoryToken(User),
           useValue: {
-            find: jest.fn().mockResolvedValue(userArray),
+            find: jest.fn().mockResolvedValue(mockUsers),
             findOneBy: jest.fn().mockResolvedValue(oneUser),
             save: jest.fn().mockResolvedValue(oneUser),
             remove: jest.fn(),
@@ -55,6 +64,7 @@ describe('UserService', () => {
   describe('create()', () => {
     it('should successfully insert a user', () => {
       const oneUser = {
+        id: 1,
         userName: 'userName1',
         firstName: 'firstName #1',
         lastName: 'lastName #1',
@@ -73,7 +83,7 @@ describe('UserService', () => {
   describe('findAll()', () => {
     it('should return an array of users', async () => {
       const users = await service.findAll();
-      expect(users).toEqual(userArray);
+      expect(users).toEqual(mockUsers);
     });
   });
 
@@ -91,6 +101,43 @@ describe('UserService', () => {
       const retVal = await service.remove(2);
       expect(removeSpy).toBeCalledWith(2);
       expect(retVal).toBeUndefined();
+    });
+  });
+
+  describe('findUsersByIds', () => {
+    it('should return users matching the given IDs', async () => {
+      const userIds = [1, 2];
+      jest
+        .spyOn(repository, 'find')
+        .mockResolvedValue([mockUsers[0] as User, mockUsers[1] as User]);
+      const result = await service.findUsersByIds(userIds);
+
+      expect(result).toEqual([mockUsers[0], mockUsers[1]]);
+      expect(repository.find).toHaveBeenCalledWith({
+        where: { id: In(userIds) },
+      });
+    });
+
+    it('should return an empty array if no users found', async () => {
+      const userIds = [999, 1000];
+      // Modify the mock to return an empty array when these IDs are requested
+      jest.spyOn(repository, 'find').mockResolvedValue([]);
+      const result = await service.findUsersByIds(userIds);
+
+      expect(result).toEqual([]);
+      expect(repository.find).toHaveBeenCalledWith({
+        where: { id: In(userIds) },
+      });
+    });
+
+    it('should return all users if all IDs are present', async () => {
+      const userIds = [1, 2, 3];
+      const result = await service.findUsersByIds(userIds);
+
+      expect(result).toEqual(mockUsers);
+      expect(repository.find).toHaveBeenCalledWith({
+        where: { id: In(userIds) },
+      });
     });
   });
 });
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { In, Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './user.entity';
 
 @Injectable()
 export class UsersService {
+  // [x: string]: any;
   constructor(
     @InjectRepository(User)
     private readonly usersRepository: Repository<User>,
@@ -30,5 +31,11 @@ export class UsersService {
 
   async remove(id: number): Promise<void> {
     await this.usersRepository.delete(id);
+  }
+
+  async findUsersByIds(userIds: number[]): Promise<User[]> {
+    return await this.usersRepository.find({
+      where: { id: In(userIds) },
+    });
   }
 }
